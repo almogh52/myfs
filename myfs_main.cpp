88addf6da1d5b321e9797cb68b3a702de493d6dd
Almog Hamdani <almogh52@gmail.com>
2019-03-28 14:09:00 +0200

    Add tree command to help

--- myfs_main.cpp
+++ myfs_main.cpp
@@ -19,7 +19,7 @@ const std::string TREE_CMD = "tree";
 const std::string HELP_CMD = "help";
 const std::string EXIT_CMD = "exit";
 
-const std::string HELP_STRING = "The following commands are supported: \n" + LIST_CMD + " [<directory>] - list directory content. \n" + CHANGE_DIRECTORY_CMD + " [<directory>] - change directory. \n" + CONTENT_CMD + " <path> - show file content. \n" + CREATE_FILE_CMD + " <path> - create empty file. \n" + CREATE_DIR_CMD + " <path> - create empty directory. \n" + EDIT_CMD + " <path> - re-set file content. \n" + HELP_CMD + " - show this help messege. \n" + EXIT_CMD + " - gracefully exit. \n";
+const std::string HELP_STRING = "The following commands are supported: \n" + LIST_CMD + " [<directory>] - list directory content. \n" + CHANGE_DIRECTORY_CMD + " [<directory>] - change directory. \n" + CONTENT_CMD + " <path> - show file content. \n" + CREATE_FILE_CMD + " <path> - create empty file. \n" + CREATE_DIR_CMD + " <path> - create empty directory. \n" + EDIT_CMD + " <path> - re-set file content. \n" + TREE_CMD + " - show the hierarchy of the file system. \n" + HELP_CMD + " - show this help messege. \n" + EXIT_CMD + " - gracefully exit. \n";
 
 std::vector<std::string> split_cmd(std::string cmd)
 {
